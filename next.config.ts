e3b89d82a4d524bc8734868731254bdd5d8c3297
Anderson Lopes <developesanderson@gmail.com>
2025-11-23 15:13:33 -0300

    fix: desabilitar Sentry no Netlify temporariamente
    - Remover outputFileTracing inválido
    - Desabilitar Sentry no Netlify (process.env.NETLIFY)
    - Isolar problema do middleware.js.nft.json
    - Sentry pode estar causando o erro de file tracing

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,7 @@ const s3Domain = process.env.S3_BUCKET
 
 const nextConfig = {
   typedRoutes: false,
-  // Desabilitar output file tracing para Netlify (causa erro com middleware)
-  outputFileTracing: false,
   // output: 'standalone' é para Docker, não Netlify
-  // Remover ou usar condicionalmente
   ...(process.env.DOCKER_BUILD === 'true' && { output: 'standalone' }),
   images: {
     // Padrão de domínios remotos mais seguro (Next.js 16+)
@@ -55,38 +52,43 @@ const nextConfig = {
   },
 }
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// Desabilitar Sentry temporariamente no Netlify para isolar o problema
+const shouldUseSentry = process.env.NETLIFY !== 'true'
 
-  org: 'devlops',
+export default shouldUseSentry
+  ? withSentryConfig(nextConfig, {
+      // For all available options, see:
+      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  project: 'javascript-nextjs',
+      org: 'devlops',
 
-  // Desabilitar upload se não houver token (Netlify)
-  authToken: process.env.SENTRY_AUTH_TOKEN,
+      project: 'javascript-nextjs',
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+      // Desabilitar upload se não houver token (Netlify)
+      authToken: process.env.SENTRY_AUTH_TOKEN,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+      // Only print logs for uploading source maps in CI
+      silent: !process.env.CI,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: '/monitoring',
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+      // This can increase your server load as well as your hosting bill.
+      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+      // side errors will fail.
+      tunnelRoute: '/monitoring',
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-})
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+
+      // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+      // See the following for more information:
+      // https://docs.sentry.io/product/crons/
+      // https://vercel.com/docs/cron-jobs
+      automaticVercelMonitors: true,
+    })
+  : nextConfig
